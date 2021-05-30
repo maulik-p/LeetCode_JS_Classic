33ba9013bd1f06675b9c8510d494c381b53c957a
Maulik Patel <work.maulikp@gmail.com>
2021-05-30 12:53:05 +0530

    :construction:  completed 1st and 2nd problem

--- exercises/LC1_TwoSum/index.js
+++ exercises/LC1_TwoSum/index.js
@@ -6,6 +6,39 @@
 // twoSum([2,7,11,15], 9) --> [0,1]
 // twoSum([3,5,4], 9) --> [1,2]
 
-function twoSum(arr, target) {}
+/**
+ * Time Complexity:  O(n^2)
+ * Space Complexity: Constant
+ * @param {*} arr
+ * @param {*} target
+ * @returns
+ */
+function twoSum(arr, target) {
+  // Iterate over all elements
+  let found = false;
+  let indexes = {
+    start: -1,
+    end: -1,
+  };
+
+  // Compare each element in array
+  for (let i = 0; i < arr.length; i++) {
+    // Check with next Elements
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        indexes.start = i;
+        indexes.end = j;
+        found = true;
+        break;
+      }
+    }
+
+    if (found) {
+      break;
+    }
+  }
+
+  return [indexes.start, indexes.end];
+}
 
 module.exports = twoSum;
--- exercises/LC2_addTwoNumbers/index.js
+++ exercises/LC2_addTwoNumbers/index.js
@@ -13,6 +13,46 @@ class ListNode {
   }
 }
 
-function addTwoNumbers(l1, l2) {}
+function linkToPreviousNode(number, index) {
+  if (number.length > 1) {
+    number[index - 1].next = number[index];
+  }
+}
+/**
+ * Time Complexity:  O(max(l1.length, l2.length))
+ * Space Complexity: sum(number l1 + number l2)
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @returns
+ */
+function addTwoNumbers(l1, l2) {
+  let number = [];
+  let carry = 0;
+  let index = 0;
+  while (l1 !== null || l2 !== null) {
+    let tempSum = ((l1 && l1.val) || 0) + ((l2 && l2.val) || 0) + +carry;
+    // Reset Carry
+    carry = 0;
+
+    // Push last digit of sum
+    number.push(new ListNode(+tempSum % 10, null));
+    linkToPreviousNode(number, index);
+
+    // Take out Carry if any
+    carry = Math.floor(tempSum / 10);
+
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
+    index++;
+  }
+
+  // Add last carry if any
+  if (carry) {
+    number.push(new ListNode(+carry), null);
+    linkToPreviousNode(number, index);
+  }
+
+  return number[0];
+}
 
 module.exports = addTwoNumbers;
